f5f0f5879f23b74451a342c11c79ecb9a218e1e2
jduyon <duyon.jacob@gmail.com>
2015-02-08 16:07:07 -0500

    something is off with the pivot element comparison

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,33 @@
 
+
+a = [4,5,1,3,9,10]
+
+
+def choose_pivot(a,n):
+    return 1
+
+def quicksort(a,length):
+    if length <= 1:
+        return
+    else:
+        p = choose_pivot(a,length)
+        left_a = a[:p]
+        right_a = a[p:]
+        quicksort(left_a,len(left_a))
+        quicksort(right_a,len(right_a))
+        new_left = []
+        new_right = []
+        i=0
+        j=0
+        while i < len(right_a) and j < len(left_a):
+            if right_a[i] > a[p]:
+                print right_a[i],a[p]
+                new_right.append(right_a[i])
+                i += 1
+            else:
+                print left_a[i],a[p]
+                new_left.append(left_a[i])
+                j += 1
+        return [new_left,new_right]
+
+print quicksort(a,6)
